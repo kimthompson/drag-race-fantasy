6c9f8e4e88d75d5b89ab43c36c01f9516e29fbce
Kim Thompson <kthompson@mpr.org>
2019-03-11 21:08:45 -0500

    well i tried

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Roger Roger')
+fetch('https://sheetsu.com/apis/v1.0bu/1f7490f595d1')
+.then(function(response) {
+  return response.json();
+})
+.then(function(json) {
+  console.log(json);
+  let data = json
+});
