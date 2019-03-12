e1beef4534d7b1738dc87eed2000eb7683d4f735
Kim Thompson <kthompson@mpr.org>
2019-03-11 20:54:13 -0500

    what if i did something simpler though?

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template lang='pug'>
+  #app
+    Scoreboard
+</template>
+
+<script>
+import Scoreboard from './components/Scoreboard';
+
+export default {
+  components: {
+    Scoreboard
+  }
+}
+</script>
--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -4,8 +4,33 @@
 
 <script>
 export default {
-  
+  data() {
+    return {
+      scores: []
+    }
+  },
+  mounted() {
+    this.getScores()
+  },
+  methods: {
+    getScores() {
+      var self = this
+      fetch('https://sheetsu.com/apis/v1.0bu/1f7490f595d1')
+      .then(function(res) {
+        self.scores = res.json()
+        console.log(res.json())
+      })
+    }
+  }
 }
+
+// fetch('https://sheetsu.com/apis/v1.0bu/1f7490f595d1')
+// .then(function(res) {
+//   return res.json();
+// })
+// .then(function(json) {
+//   console.log(json);
+// });
 </script>
 
 <style lang="stylus" scoped>
